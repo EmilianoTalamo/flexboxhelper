535d2711888e0af66dc3da8f904d9a63e673e9c0
EmilianoTalamo <emilianotalamo@gmail.com>
2017-03-30 19:58:08 -0300

    Tab OCDs

--- flex.js
+++ flex.js
@@ -4,24 +4,29 @@ var flexdirection = document.getElementById("flexdirection");
 var flexwrap = document.getElementById("flexwrap");
 var justifycontent = document.getElementById("justifycontent");
 
-aligncontent.addEventListener("change", function (evt) {
-  document.getElementById("cajitas").style.alignContent = evt.target.value;
+aligncontent.addEventListener("change", function (evt)
+{
+	document.getElementById("cajitas").style.alignContent = evt.target.value;
 });
 
-alignitems.addEventListener("change", function (evt) {
-  document.getElementById("cajitas").style.alignItems = evt.target.value;
+alignitems.addEventListener("change", function (evt)
+{
+	document.getElementById("cajitas").style.alignItems = evt.target.value;
 });
 
-flexdirection.addEventListener("change", function (evt) {
-  document.getElementById("cajitas").style.flexDirection = evt.target.value;
+flexdirection.addEventListener("change", function (evt)
+{
+	document.getElementById("cajitas").style.flexDirection = evt.target.value;
 });
 
-flexwrap.addEventListener("change", function (evt) {
-  document.getElementById("cajitas").style.flexWrap = evt.target.value;
+flexwrap.addEventListener("change", function (evt)
+{
+	document.getElementById("cajitas").style.flexWrap = evt.target.value;
 });
 
-justifycontent.addEventListener("change", function (evt) {
-  document.getElementById("cajitas").style.justifyContent = evt.target.value;
+justifycontent.addEventListener("change", function (evt)
+{
+	document.getElementById("cajitas").style.justifyContent = evt.target.value;
 });
 
 function handleChange(checkbox)
