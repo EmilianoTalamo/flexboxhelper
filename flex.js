53470baf8ee599e82e8cd751338de86f07c2d0d4
EmilianoTalamo <emilianotalamo@gmail.com>
2017-03-30 19:38:26 -0300

    initial commit

---
+++ flex.js
@@ -0,0 +1,37 @@
+var aligncontent = document.getElementById("aligncontent");
+var alignitems = document.getElementById("alignitems");
+var flexdirection = document.getElementById("flexdirection");
+var flexwrap = document.getElementById("flexwrap");
+var justifycontent = document.getElementById("justifycontent");
+
+aligncontent.addEventListener("change", function (evt) {
+  document.getElementById("cajitas").style.alignContent = evt.target.value;
+});
+
+alignitems.addEventListener("change", function (evt) {
+  document.getElementById("cajitas").style.alignItems = evt.target.value;
+});
+
+flexdirection.addEventListener("change", function (evt) {
+  document.getElementById("cajitas").style.flexDirection = evt.target.value;
+});
+
+flexwrap.addEventListener("change", function (evt) {
+  document.getElementById("cajitas").style.flexWrap = evt.target.value;
+});
+
+justifycontent.addEventListener("change", function (evt) {
+  document.getElementById("cajitas").style.justifyContent = evt.target.value;
+});
+
+function handleChange(checkbox)
+{
+	if(checkbox.checked == true)
+	{
+		document.getElementById("cajitas").style.display = 'flex';
+	}
+	else
+	{
+		document.getElementById("cajitas").style.display = 'block';
+  }
+}
